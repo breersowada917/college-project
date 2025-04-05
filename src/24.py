21da55f8e42fb64d247a47cc5315cda67653c1ba
breersowada917 <breersowada917@gmail.com>
2025-04-05 06:43:03 +0000

    Add 24.py

---
+++ src/24.py
@@ -0,0 +1,15 @@
+def add_numbers(a: int, b: int) -> int:
+    """
+    Add two numbers
+
+    Parameters:
+    a (int): First number
+    b (int): Second number
+
+    Returns:
+    int: Sum of a and b
+    """
+    return a + b
+
+result = add_numbers(5, 3)
+print(result)

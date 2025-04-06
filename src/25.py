a691cbf54bbd96b715213ae51c0e73e8f5660b3a
breersowada917 <breersowada917@gmail.com>
2025-04-06 12:45:33 +0000

    Add 25.py

---
+++ src/25.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+# Example DataFrame
+data = {
+    'name': ['John', 'Anna', 'Michael'],
+    'age': [28, 35, 40],
+    'country': ['USA', 'UK', 'Australia']
+}
+df = pd.DataFrame(data)
+
+print(df)
